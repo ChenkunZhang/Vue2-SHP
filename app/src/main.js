814ec2d014894208743111e90f339453d0728e84
Le Zhang <111129174+ChenkunZhang@users.noreply.github.com>
2024-06-04 13:29:14 +0800

    Header,Footer

--- app/src/main.js
+++ app/src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
 
+Vue.config.productionTip = false  // Disabling the production tip
+
+// Defining the routes
 new Vue({
   render: h => h(App),
 }).$mount('#app')
