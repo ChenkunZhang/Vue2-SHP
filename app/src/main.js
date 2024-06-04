9fe72d16cd1b42cd4813beb71d7cfff534173e6c
Le Zhang <111129174+ChenkunZhang@users.noreply.github.com>
2024-06-04 17:26:19 +0800

    添加TypeNav静态组件

--- app/src/main.js
+++ app/src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from '@/App.vue'
 import router from '@/router';
+import TypeNav from '@/components/TypeNav'
 
 Vue.config.productionTip = false  // Disabling the production tip
+
+Vue.component('TypeNav', TypeNav) // Registering the TypeNav component globally
 
 new Vue({
   render: h => h(App),
