256799c4fc6ffc259acda02d757edeec8748fff9
Le Zhang <111129174+ChenkunZhang@users.noreply.github.com>
2024-06-08 20:30:40 +0800

    格式化api文件

--- app/src/api/api.js
+++ app/src/api/api.js
@@ -3,7 +3,4 @@ import request from "./request";
 // API接口统一管理 TypeNav 三级菜单接口 /api/product/getBaseCategoryList GET
 export function reqBaseCategoryList() {
   return request({ url: "/product/getBaseCategoryList", method: "get" });
-
-
-
 }
