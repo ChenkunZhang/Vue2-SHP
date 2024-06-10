32c2e050b308b02e879dd126958de4da6e3b9e07
Le Zhang <111129174+ChenkunZhang@users.noreply.github.com>
2024-06-10 13:20:47 +0800

    Update App.vue

--- app/src/App.vue
+++ app/src/App.vue
@@ -5,7 +5,6 @@
     <!-- 路由出口 -->
     <router-view></router-view>
     <!-- 底部 -->
-    <Footer v-show='$route.meta.show'></Footer>
     <!-- 在Home与Search可见的，但是Login|Register不可见 -->
     <!-- 利用路由元信息解决当前问题好处：一行代码就可以解决 -->
     <Footer v-show="$route.meta.show"></Footer>
@@ -13,25 +12,20 @@
 </template>
 
 <script>
-import Header from '@/components/Header'
-import Footer from '@/components/Footer'
 //引入Header与Footer非路由组件
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
 export default {
-  name: 'App',
   name: "App",
   components: {
     Header,
-    Footer
     Footer,
   },
   mounted(){
     //异步获取所有分类列表数据.在App组件中执行, 减少请求的次数.
     this.$store.dispatch("getNavData");
   }
-}
 };
 </script>
 
