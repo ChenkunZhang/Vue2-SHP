b54108c443f9ac2845b391e38339312d76b4c008
Le Zhang <111129174+ChenkunZhang@users.noreply.github.com>
2024-06-10 13:20:13 +0800

    Update App.vue

--- app/src/App.vue
+++ app/src/App.vue
@@ -6,20 +6,28 @@
     <router-view></router-view>
     <!-- 底部 -->
     <Footer v-show='$route.meta.show'></Footer>
+    <!-- 在Home与Search可见的，但是Login|Register不可见 -->
+    <!-- 利用路由元信息解决当前问题好处：一行代码就可以解决 -->
+    <Footer v-show="$route.meta.show"></Footer>
   </div>
 </template>
 
 <script>
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
+//引入Header与Footer非路由组件
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 export default {
   name: 'App',
+  name: "App",
   components: {
     Header,
     Footer
   }
 }
+};
 </script>
 
 <style>
