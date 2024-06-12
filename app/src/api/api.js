23bd21d3ab5d6139edb3f07fe08761a77ac1175b
Le Zhang <111129174+ChenkunZhang@users.noreply.github.com>
2024-06-12 23:21:50 +0800

    搜索接口

--- app/src/api/api.js
+++ app/src/api/api.js
@@ -6,6 +6,12 @@ export function reqBaseCategoryList() {
   return service({ url: "/product/getBaseCategoryList", method: "get" });
 }
 
+// API接口统一管理 ProductList 商品列表接口 /api/product/list POST
+// 当前这个接口（获取搜索模块的数据），给服务器传递一个默认参数【至少是一个空对象】
+export function reqProductList(searchParams) {
+  return service({ url: "/list", method: "post", data: searchParams });
+}
+
 // API接口统一管理 mock数据
 // 获取banner（Home首页轮播图接口）
 export function reqBannerList() {
