b671cfae3b8feb82280d5218d9fd84c955c69234
Le Zhang <111129174+ChenkunZhang@users.noreply.github.com>
2024-06-12 23:19:46 +0800

    Update vue.config.js

--- app/vue.config.js
+++ app/vue.config.js
@@ -1,17 +1,17 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
   transpileDependencies: true,
   // 关闭eslint
   lintOnSave: false,
   // 配置代理跨域
-   devServer: {
+  devServer: {
     proxy: {
-      '/api': {
-        target: 'http://gmall-h5-api.atguigu.cn' 
-        }
-      }
-    }
-})
+      "/api": {
+        target: "http://gmall-h5-api.atguigu.cn",
+      },
+    },
+  },
+});
 
 //接口地址会更新，失效查看 https://www.bilibili.com/video/BV1Vf4y1T7bw?p=17&vd_source=928500936bdd049604bdbc03246ed8ae
