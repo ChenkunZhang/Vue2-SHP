6b479b26ab1eb7dc4b211df9b86dfe1937701d88
Le Zhang <111129174+ChenkunZhang@users.noreply.github.com>
2024-06-19 23:19:29 +0800

    Update routes.js

--- app/src/router/routes.js
+++ app/src/router/routes.js
@@ -46,22 +46,22 @@ export default [
     meta: {
       show: true,
     },
-    beforeEnter (to, from, next) {
-      // 得到当前路由信息对象
-      // const route = router.currentRoute  // route就是from
+    // beforeEnter (to, from, next) {
+    //   // 得到当前路由信息对象
+    //   // const route = router.currentRoute  // route就是from
 
-      // 得到要跳转到目路由的query参数
-      const skuNum = to.query.skuNum
-      // 读取保存的数据
-      const skuInfo = JSON.parse(window.sessionStorage.getItem('SKU_INFO_KEY'))
-      console.log('---', skuNum, skuInfo)
-      // 只有都存在, 才放行
-      if (skuNum && skuInfo) {
-        next()
-      } else { // 在组件对象创建前强制跳转到首页
-        next('/')
-      }
-    }
+    //   // 得到要跳转到目路由的query参数
+    //   const skuNum = to.query.skuNum
+    //   // 读取保存的数据
+    //   const skuInfo = JSON.parse(window.sessionStorage.getItem('SKU_INFO_KEY'))
+    //   console.log('---', skuNum, skuInfo)
+    //   // 只有都存在, 才放行
+    //   if (skuNum && skuInfo) {
+    //     next()
+    //   } else { // 在组件对象创建前强制跳转到首页
+    //     next('/')
+    //   }
+    // }
   },
   {
     path: "/shopcart",
